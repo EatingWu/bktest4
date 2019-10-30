a240fcd490faa2729792449ecd84d5947d3b855d
EatingWu <10ytwu@gmail.com>
2019-10-30 21:40:02 +0800

    3rd commit

--- conf/default.py
+++ conf/default.py
@@ -31,9 +31,9 @@ from django.conf.global_settings import *  # noqa
 # ==============================================================================
 # 在蓝鲸智云开发者中心 -> 点击应用ID -> 基本信息 中获取 APP_ID 和 APP_TOKEN 的值
 APP_ID = 'bktest4'
-APP_TOKEN = '64fcf709-0bb6-424c-a777-101d76b9fa14'
+APP_TOKEN = 'ebc7e997-48e1-47c6-af65-e0bdf51e88cf'
 # 蓝鲸智云开发者中心的域名，形如：http://paas.bking.com
-BK_PAAS_HOST = 'http://http://paas.bk5.com/'
+BK_PAAS_HOST = 'http://paas.bk8.com/'
 
 # 请求官方 API 默认版本号，可选值为："v2" 或 ""；其中，"v2"表示规范化API，""表示未规范化API
 DEFAULT_BK_API_VER = 'v2'
